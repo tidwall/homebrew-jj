89852b2a148bc59a1b944da57068a80bcef4a8f4
Josh Baker <joshbaker77@gmail.com>
2017-12-18 05:58:27 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# homebrew-jj
+
+for homesbrew taps formula
+
+## add homebrew taps
+
+```
+brew tap tidwall/jj
+```
+
+## install jj by homebrew
+
+```
+brew install jj
+```
---
+++ jj.rb
@@ -0,0 +1,18 @@
+HOMEBREW_JJ_VERSION = "1.0.0".freeze
+HOMEBREW_JJ_SHA = "0595f69f2bb79bbcc15abbae576bab73adc18dd0ba66e2cbd1e870a6894aeccd".freeze
+
+class Jj < Formula
+  desc "jj"
+  homepage "https://github.com/tidwall/jj"
+  sha256 HOMEBREW_JJ_SHA
+  url "#{homepage}/releases/download/#{HOMEBREW_JJ_VERSION}/jj-#{HOMEBREW_JJ_VERSION}-darwin-amd64.zip"
+  version HOMEBREW_JJ_VERSION
+
+  def install
+    bin.install "jj"
+  end
+
+  test do
+    system "false"
+  end
+end
