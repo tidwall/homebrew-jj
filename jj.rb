c86929bc401cdaf58061f624daf7bd73f69986ea
Josh Baker <joshbaker77@gmail.com>
2017-12-18 06:40:29 -0700

    fix version

--- jj.rb
+++ jj.rb
@@ -1,11 +1,11 @@
-HOMEBREW_JJ_VERSION = "v1.0.0".freeze
+HOMEBREW_JJ_VERSION = "1.0.0".freeze
 HOMEBREW_JJ_SHA = "0595f69f2bb79bbcc15abbae576bab73adc18dd0ba66e2cbd1e870a6894aeccd".freeze
 
 class Jj < Formula
   desc "jj"
   homepage "https://github.com/tidwall/jj"
   sha256 HOMEBREW_JJ_SHA
-  url "#{homepage}/releases/download/#{HOMEBREW_JJ_VERSION}/jj-#{HOMEBREW_JJ_VERSION}-darwin-amd64.zip"
+  url "#{homepage}/releases/download/v#{HOMEBREW_JJ_VERSION}/jj-#{HOMEBREW_JJ_VERSION}-darwin-amd64.zip"
   version HOMEBREW_JJ_VERSION
 
   def install
