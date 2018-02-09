21caefec858ff4a2c1858d7debd52cd9f344519e
Josh Baker <joshbaker77@gmail.com>
2018-02-09 16:01:13 -0700

    Update jj.rb

--- jj.rb
+++ jj.rb
@@ -1,5 +1,5 @@
-HOMEBREW_JJ_VERSION = "1.1.2".freeze
-HOMEBREW_JJ_SHA = "bf6fcf05b1d969c86df778ad02ea4c5735af3e0a7cae29315706bf02cc76575b".freeze
+HOMEBREW_JJ_VERSION = "1.2.0".freeze
+HOMEBREW_JJ_SHA = "fc2b3e90d378ec7824a4c8c4e2ad7450b0e68f571397fecaa3c3d090309b3326".freeze
 
 class Jj < Formula
   desc "jj"
